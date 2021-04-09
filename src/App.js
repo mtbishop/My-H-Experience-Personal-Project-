ca2a533770e813db681a286350c6ee9181433e83
mtbishop <bishopmatthew76@gmail.com>
2021-04-08 21:35:39 -0500

    started react conversion

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./components/Header"
+import Times from "./components/Times"
+import Videos from "./components/Videos"
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Times/>
+      <Videos/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Videos.js
@@ -0,0 +1,393 @@
+import React from 'react';
+
+function Videos() {
+  return (
+      <div>
+              <div id="videoRow" className="row">
+            <div id="haloRuns">
+                <h1 id="haloReach">HALO REACH</h1>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="winterContingency">Winter Contingency</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="oniSwordBase">ONI: Sword Base</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="nightfall">Nightfall</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+
+            <div className="col-lg-4">
+                <p className="card-text" id="tipOfTheSpear">Tip of the Spear</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="longNightOfSolace">Long Night of Solace</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="exodus">Exodus</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="newAlexandria">New Alexandria</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="thePackage">The Package</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="thePillarOfAutumn">The Pillar of Autumn</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+        </div>
+        <div id="videoRow" className="row">
+            <div id="haloRuns">
+                <h1 id="haloCE">HALO CE</h1>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="ceThePillarofAutumn">The Pillar of Autumn</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="ceHalo">Halo</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="theTruthAndReconciliation">The Truth and Reconciliation</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+
+            <div className="col-lg-4">
+                <p className="card-text" id="theSilentCartographer">The Silent Cartographer</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="assaultOnTheControlRoom">Assault on the Control Room</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="guiltySpark343">343 Guilty Spark</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="theLibrary">The Library</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="twoBetrayals">Two Betrayals</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="theMaw">The Maw</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+        </div>
+        <div id="videoRow" className="row">
+            <div id="haloRuns">
+                <h1 id="halo2">HALO 2</h1>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="carioStation">Cario Station</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="outskirts">Outskirts</p>
+                <div className="card" style={{width:"18rem"}}>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/xhL78vBpfi8" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="metropolis">Metropolis</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="theArbiter">The Arbiter</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="theOracle">The Oracle</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="deltaHalo">Delta Halo</p>
+                <div className="card" style={{width:"18rem"}}>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/d3GO6-BvMTE" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="regret">Regret</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="sacredIcon">Sacred Icon</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="quarantineZone">Quarantine Zone</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="gravemind">Gravemind</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="uprising">Uprising</p>
+                <div className="card" style={{width:"18rem"}}>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/DnizpdFaCBQ" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="highCharity">High Charity</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="theGreatJourney">The Great Journey</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+        </div>
+        <div id="videoRow" className="row">
+            <div id="haloRuns">
+                <h1 id="halo3">HALO 3</h1>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">Sierra 117</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="deltaHalo">Crow's Nest</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="uprising">Tsavo Highway</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+
+            <div className="col-lg-4">
+                <p className="card-text" id="">The Storm</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">Floodgate</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">The Ark</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">The Covenant</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">Cortana</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">Halo</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+        </div>
+        <div id="videoRow" className="row">
+            <div id="haloRuns">
+                <h1 id="halo3Odst">HALO 3: ODST</h1>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">Tayari Plaza</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="deltaHalo">Uplift Reserve</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="uprising">Kizingo Boulevard</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+
+            <div className="col-lg-4">
+                <p className="card-text" id="">ONI Alpha Site</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">NMPD HQ</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">Kikowani Station</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">Data Hive</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">Coastal Highway</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+        </div>
+        <div id="videoRow" className="row">
+            <div id="haloRuns">
+                <h1 id="halo4">HALO 4</h1>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">Dawn</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="deltaHalo">Requiem</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="uprising">Forerunner</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+
+            <div className="col-lg-4">
+                <p className="card-text" id="">Infinity</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">Reclaimer</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">Shutdown</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">Composer</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <p className="card-text" id="">Midnight</p>
+                <div className="card" style={{width:"18rem"}}>
+                    {/* <!-- Video here --> */}
+                </div>
+            </div>
+        </div>
+    </div>
+
+  );
+}
+
+export default Videos;
